edd119436a1179edc220e5a6ac3f1c454bd6144b
lovaire <rifsrahmt@gmail.com>
2025-10-28 17:38:10 +0700

    atmiiin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    
+    # Ini akan tampil di daftar user
+    list_display = ['username', 'email', 'display_name', 'is_staff']
+    
+    # Ini untuk menambah 'display_name' saat edit user
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('display_name',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('display_name',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,28 @@
+# products/admin.py
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    # Kolom apa saja yang tampil di daftar produk
+    list_display = ('name', 'affiliator', 'category', 'marketplace', 'price', 'created_at')
+    
+    # Menambah filter di sisi kanan
+    list_filter = ('category', 'marketplace', 'affiliator')
+    
+    # Menambah kotak pencarian
+    search_fields = ('name', 'description', 'store_name')
+    
+    # Mengatur urutan field saat mengedit
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'affiliator', 'price')
+        }),
+        ('Kategorisasi', {
+            'fields': ('category', 'marketplace', 'store_name')
+        }),
+        ('Detail Link', {
+            'fields': ('product_link', 'image_urls', 'description', 'rating')
+        }),
+    )
+
+admin.site.register(Product, ProductAdmin)
